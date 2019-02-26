debbd0f669d0b4c1fd48ef8549d7abce5ff3e770
john-primitive <john@primitive.io>
2019-02-26 20:21:18 +0000

    add graphics util classes

---
+++ Assets/Circle.cs
@@ -0,0 +1,301 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.GraphicsUtil.Shapes
+{
+    public class Circle : Polygon
+    {
+        // the amount of straight lines per 1 meter of an arc; 
+        public int sidePer1M = 16;
+
+        // the current radius and percent completion of a circle or ring;
+        public float curPrct;
+
+        public float curR;
+
+        public void Draw(float radius)
+        {
+            // Draws a full circle (100%) with no height (not a cylinder)
+            DrawCirc(radius, 1, 0);
+        }
+
+        public void DrawCirc(float R, float prct, float h, float offset = 0f)
+        {
+            curPrct = prct;
+            curR = R;
+
+            // calculate the number of sides to this circle;
+            // there is one less side because a full circle fills the last side;
+            int side = Mathf.RoundToInt(Mathf.Pow(R + 1f, .8f) * sidePer1M * prct);
+            if (side <= 0)
+                return;
+
+            // if an incomplete circle, draw one more side;
+            if (prct < 1f)
+                side += 1;
+
+            // generate vertices and indices;
+            Vector3[] skinList = new Vector3[side + 1];
+            int[] indList = new int[side * 3];
+
+            skinList[0] = Vector3.zero;
+            float alpha = 360f / (side - (prct < 1f ? 1 : 0)); // a positive alpha is drawn clockwise;
+
+            int count = 1;
+            int[] insInd;
+            for (int ii = side - 1; ii >= 0; ii--)
+            {
+                skinList[count] = VectArc(R, ii * alpha, prct, offset, 0f, 0f);
+                insInd = new[] {0, count, count + 1};
+                insInd.CopyTo(indList, (count - 1) * 3);
+                count++;
+            }
+
+            indList[indList.Length - 1] = 1;
+
+            if (h < float.Epsilon)
+            {
+                // a circle with 0 depth - only draw two sides;
+                Draw3DPoly(skinList, MirrorIndices(indList, 0));
+            }
+            else
+            {
+                Extrude(skinList, indList, h, true, true, 0f);
+            }
+        }
+
+        public void DrawRing(float R1, float R2, float prct, float h)
+        {
+            curR = (R1 + R2) * .5f;
+            prct = Mathf.Abs(prct);
+            curPrct = prct;
+
+            // calculate the number of sides to this ring;
+            // there is one less side because a full ring fills the last side;
+            int side = Mathf.RoundToInt(Mathf.Pow(R1 + 1f, .8f) * sidePer1M * prct);
+            if (side <= 0)
+                return;
+
+            // if an incomplete ring, draw one more side;
+            if (prct < 1f)
+                side += 1;
+
+            // generate vertices and indices;
+            Vector3[] skinList = new Vector3[side * 2];
+            int[] indList = new int[side * 6];
+            int[] insInd;
+            float alpha = 360f / (side - (prct < 1f ? 1 : 0)); // a positive alpha is drawn clockwise;
+
+            for (int ii = 0; ii < side; ii++)
+            {
+                skinList[ii] = VectArc(R1, ii * alpha, prct, 0f, 0f, 0f);
+                skinList[side * 2 - ii - 1] = VectArc(R2, ii * alpha, prct, 0f, 0f, 0f);
+
+                insInd = new[] {ii, side * 2 - ii - 2, side * 2 - ii - 1};
+                insInd.CopyTo(indList, ii * 6);
+
+                insInd = new[] {ii, ii + 1, side * 2 - ii - 2};
+                insInd.CopyTo(indList, ii * 6 + 3);
+            }
+
+            if (prct >= 1f - float.Epsilon)
+            {
+                // if a complete ring - connect the last segment of the ring to the beginning segment;
+                indList[indList.Length - 1] = side * 2 - 1;
+                indList[indList.Length - 2] = 0;
+
+                indList[indList.Length - 5] = side * 2 - 1;
+            }
+            else if (indList.Length > 6)
+            {
+                // remove the last segment of the incomplete ring;
+                int[] truncList = new int[indList.Length - 3];
+                for (int zz = 0; zz < truncList.Length; zz++)
+                    truncList[zz] = indList[zz];
+
+                indList = truncList;
+            }
+
+            if (h < float.Epsilon)
+            {
+                // a ring with 0 depth - only draw two sides;
+                Draw3DPoly(skinList, MirrorIndices(indList, 0));
+            }
+            else
+            {
+                Extrude(skinList, indList, h, false, true, 0f);
+            }
+        }
+
+        static Vector3 VectArc(float R, float alpha, float prct, float offSet, float cX, float cZ)
+        {
+            return new Vector3(
+                R * Mathf.Sin(
+                    alpha * prct * Mathf.PI / 180f +
+                    offSet * 2f * Mathf.PI) +
+                cX,
+                0f,
+                R * Mathf.Cos(
+                    alpha * prct * Mathf.PI / 180f +
+                    offSet * 2f * Mathf.PI) +
+                cZ
+            );
+        }
+
+        static object[] SprockTick(float R, float H, float alpha, float aDelt, float sprockTh, float baseAl,
+            float pointAl, int counter, float spiralH)
+        {
+            List<Vector3> pointList = new List<Vector3>();
+            List<int> indList = new List<int>();
+
+            pointList.Add(new Vector3(R * Mathf.Sin(alpha + baseAl), spiralH, R * Mathf.Cos(alpha + baseAl)));
+            pointList.Add(new Vector3((R - sprockTh) * Mathf.Sin(alpha + baseAl), spiralH,
+                (R - sprockTh) * Mathf.Cos(alpha + baseAl)));
+
+            pointList.Add(new Vector3((R - H) * Mathf.Sin(alpha + pointAl), spiralH,
+                (R - H) * Mathf.Cos(alpha + pointAl)));
+            pointList.Add(new Vector3((R - H) * Mathf.Sin(alpha - pointAl), spiralH,
+                (R - H) * Mathf.Cos(alpha - pointAl)));
+
+            pointList.Add(new Vector3(R * Mathf.Sin(alpha - baseAl), spiralH, R * Mathf.Cos(alpha - baseAl)));
+            pointList.Add(new Vector3((R - sprockTh) * Mathf.Sin(alpha - baseAl), spiralH,
+                (R - sprockTh) * Mathf.Cos(alpha - baseAl)));
+
+            indList.Add(counter + 0);
+            indList.Add(counter + 1);
+            indList.Add(counter + 4);
+
+            indList.Add(counter + 1);
+            indList.Add(counter + 5);
+            indList.Add(counter + 4);
+
+            indList.Add(counter + 1);
+            indList.Add(counter + 2);
+            indList.Add(counter + 5);
+
+            indList.Add(counter + 2);
+            indList.Add(counter + 3);
+            indList.Add(counter + 5);
+
+            counter += 6;
+            // in-fill;       
+            float tempA = alpha - .03f;
+            while (tempA > aDelt + .03f)
+            {
+                pointList.Add(new Vector3(R * Mathf.Sin(tempA), 0f, R * Mathf.Cos(tempA)));
+                pointList.Add(new Vector3((R - sprockTh) * Mathf.Sin(tempA), 0f, (R - sprockTh) * Mathf.Cos(tempA)));
+                indList.Add(counter - 2);
+                indList.Add(counter - 1);
+                indList.Add(counter + 0);
+
+                indList.Add(counter - 1);
+                indList.Add(counter + 1);
+                indList.Add(counter + 0);
+
+                counter += 2;
+                tempA -= .03f;
+            }
+
+            indList.Add(counter - 2);
+            indList.Add(counter - 1);
+            indList.Add(counter + 0);
+
+            indList.Add(counter - 1);
+            indList.Add(counter + 1);
+            indList.Add(counter + 0);
+
+            object[] retArr = {pointList, indList, counter};
+            return retArr;
+        }
+
+        public void DrawSprocket()
+        {
+            const float earthR = .15f;
+
+            // create point cloud for earth sprocket mesh;
+            List<Vector3> pointList = new List<Vector3>();
+            List<int> indList = new List<int>();
+            object[] retArr;
+            int pointCounter = 0;
+            int counter = 0;
+            float alpha;
+            const float sprockTh = .003f;
+            const float baseAl = .02f;
+            const float pointAl = .01f;
+            const float smallH = .007f;
+            const float bigH = .015f;
+            const float hR = earthR * .4f;
+            const int segments = 8;
+            const int subSegments = 4;
+            const float stepD = 360f / (segments * subSegments) * Mathf.PI / 180f;
+
+            for (int tt = 0; tt < segments; tt++)
+            {
+                alpha = -counter * stepD;
+                // create hour tick;
+
+                retArr = SprockTick(hR, bigH, alpha, -(counter + 1) * stepD, sprockTh, baseAl, pointAl, pointCounter,
+                    0f);
+                pointList.AddRange((List<Vector3>) retArr[0]);
+                indList.AddRange((List<int>) retArr[1]);
+                pointCounter = (int) retArr[2];
+                counter++;
+                for (int dd = 0; dd < subSegments - 1; dd++)
+                {
+                    alpha = -counter * stepD;
+                    // create 15min tick;
+                    retArr = SprockTick(hR, smallH, alpha, -(counter + 1) * stepD, sprockTh, baseAl, pointAl,
+                        pointCounter, 0f);
+                    pointList.AddRange((List<Vector3>) retArr[0]);
+                    indList.AddRange((List<int>) retArr[1]);
+                    pointCounter = (int) retArr[2];
+                    counter++;
+                }
+            }
+
+            indList.RemoveRange(indList.Count - 6, 6);
+
+            Draw3DPoly(pointList.ToArray(), MirrorIndices(indList.ToArray(), 0));
+            name = "Sprocket";
+        }
+        
+    }
+
+    public static class NewCylinder
+    {
+        public static Circle cylinder;
+
+        public static Circle paren;
+
+        public static Circle methodRing;
+        public static Circle rootRing;
+        public static Circle rootDot;
+
+        public static Circle referenceDepthIndicator;
+
+        public static void Init(PolygonFactory polygonFactory, Material mainMat)
+        {
+            Color shapeColor = Color.white;
+            
+            cylinder = PolygonFactory.NewCirclePoly(mainMat, false);
+            cylinder.sidePer1M = 8;
+            cylinder.DrawRing(1, .85f, 1, .3f);
+            cylinder.name = "cylinder";
+            cylinder.SetColor(shapeColor);
+            cylinder.transform.SetParent(polygonFactory.transform, false);
+
+            paren = PolygonFactory.NewCirclePoly(mainMat, false);
+            paren.DrawRing(.015f, .0135f, .3f, 0);
+            paren.name = "paren";
+            paren.transform.SetParent(polygonFactory.transform, false);
+
+            rootDot = PolygonFactory.NewCirclePoly(mainMat, false);
+            rootDot.name = "RootDot";
+            rootDot.DrawCirc(.035f * .5f, 1, 0);
+            rootDot.SetColor(shapeColor);
+            rootDot.transform.SetParent(polygonFactory.transform, false);
+            
+
+        }
+    }
+}
---
+++ Assets/Polygon.cs
@@ -0,0 +1,227 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.GraphicsUtil.Shapes
+{
+    public class Polygon : MonoBehaviour
+    {
+        // Persistent References
+        public MeshFilter meshFilter;
+        public Renderer rend;
+        public bool wireFrame = false;
+
+        // Link Vars
+        Coroutine colorAnimator;
+
+        #region SHAPE Functions
+
+        public void DrawRegPoly(float radius, int sides, float offSet, float h, float taper)
+        {
+            Vector3[] verts = new Vector3[sides];
+            int numTriangles = sides - 2;
+            int[] ind = new int[numTriangles * 3];
+
+            float alpha = 2 * Mathf.PI / sides;
+            for (int ii = 0; ii < sides; ii++)
+                verts[ii] = new Vector3(radius * Mathf.Sin(alpha * ii + offSet), 0,
+                    radius * Mathf.Cos(alpha * ii + offSet));
+
+            for (int ii = 0; ii < numTriangles; ii++)
+            {
+                ind[ii * 3 + 0] = 0;
+                ind[ii * 3 + 1] = ii + 1;
+                ind[ii * 3 + 2] = ii + 2;
+            }
+
+            if (h < float.Epsilon)
+            {
+                Draw3DPoly(verts, MirrorIndices(ind, 0));
+            }
+            else
+            {
+                Extrude(verts, ind, h, false, true, taper);
+            }
+        }
+
+        #endregion
+
+        #region DRAW Functions
+
+        public static int[] MirrorIndices(int[] indices, int vertLength)
+        {
+            // copy the indices twice (mirror the second set of indices);
+            int[] retIndices = new int[indices.Length * 2];
+            int subCount = 0;
+            for (int ii = 0; ii < indices.Length; ii++)
+            {
+                retIndices[ii] = indices[ii];
+                retIndices[ii + indices.Length] = indices[ii + subCount] + vertLength;
+
+                switch (subCount)
+                {
+                    case 0:
+                        subCount = 1;
+                        break;
+                    case 1:
+                        subCount = -1;
+                        break;
+                    default:
+                        subCount = 0;
+                        break;
+                }
+            }
+
+            return retIndices;
+        }
+
+        protected void Extrude(Vector3[] vertices, int[] indices, float h,
+            bool centerPoint, bool fillTopAndBottom, float taper)
+        {
+            Vector3[] extVertices = new Vector3[vertices.Length * 2];
+            // create mirror of main contour at the depth of the extrusion and copy both contours into a new vertices[];
+            for (int ii = 0; ii < vertices.Length; ii++)
+            {
+                extVertices[ii] = new Vector3(vertices[ii].x * (1 - taper), h * .5f, vertices[ii].z * (1 - taper));
+                extVertices[ii + vertices.Length] = new Vector3(vertices[ii].x * (1 + taper), -h * .5f,
+                    vertices[ii].z * (1 + taper));
+            }
+
+            // create indices;
+            int[] extIndices = new int[vertices.Length * 6];
+            int fillIndOffset = 0;
+
+            if (fillTopAndBottom)
+            {
+                extIndices = new int[indices.Length * 2 + vertices.Length * 6];
+                fillIndOffset = indices.Length * 2;
+
+                int[] mirInd = MirrorIndices(indices, vertices.Length);
+                for (int ii = 0; ii < mirInd.Length; ii++)
+                    extIndices[ii] = mirInd[ii];
+            }
+
+            // link up the two contours;
+            int mod = 0;
+            if (centerPoint)
+                mod = 1;
+            for (int ii = mod; ii < vertices.Length - 1; ii++)
+            {
+                extIndices[(ii - mod) * 6 + 0 + fillIndOffset] = ii + 0;
+                extIndices[(ii - mod) * 6 + 1 + fillIndOffset] = ii + vertices.Length;
+                extIndices[(ii - mod) * 6 + 2 + fillIndOffset] = ii + vertices.Length + 1;
+
+                extIndices[(ii - mod) * 6 + 3 + fillIndOffset] = ii + 0;
+                extIndices[(ii - mod) * 6 + 4 + fillIndOffset] = ii + vertices.Length + 1;
+                extIndices[(ii - mod) * 6 + 5 + fillIndOffset] = ii + 1;
+            }
+
+            // close external surface end to start;
+            extIndices[extIndices.Length - 1] = vertices.Length + mod;
+            extIndices[extIndices.Length - 3] = 0 + mod;
+            extIndices[extIndices.Length - 2] = vertices.Length - 1;
+            extIndices[extIndices.Length - 4] = extVertices.Length - 1;
+            extIndices[extIndices.Length - 6] = vertices.Length + mod;
+            extIndices[extIndices.Length - 5] = vertices.Length - 1;
+
+            Draw3DPoly(extVertices, extIndices);
+        }
+
+        public void Draw3DPoly(Vector3[] vertices, int[] indices)
+        {
+            // https://docs.unity3d.com/ScriptReference/Mesh.html
+
+            // Retrieve the mesh
+            Mesh sharedMesh = meshFilter.sharedMesh;
+
+            sharedMesh.Clear();
+            sharedMesh.vertices = vertices;
+            sharedMesh.triangles = indices;
+            sharedMesh.RecalculateNormals();
+            sharedMesh.RecalculateBounds();
+
+            // Set up game object with mesh;
+            meshFilter.sharedMesh = sharedMesh;
+        }
+
+        #endregion
+
+        #region ACTION Functions
+
+        /// <summary>
+        /// This brightens theme colors that are too dark on the dark background for the wireframes.
+        /// 
+        /// The real solution to this problem is that themes should include separate colors for the wireframes, 
+        /// and then we can remove this method.
+        /// </summary>
+        static Color BrightenedColor(Color color)
+        {
+            float brightnessMultiplier = (color.r + color.g + color.b) / 3;
+
+            if (color.r + color.g + color.b < 1.5f)
+            {
+                brightnessMultiplier = 2 - brightnessMultiplier;
+            }
+
+            return color * brightnessMultiplier;
+        }
+
+        void ChangeColorProperties(Color color)
+        {
+            rend.material.SetColor("_Color", color);
+
+            if (wireFrame)
+            {
+                rend.material.SetColor("_V_WIRE_Color", BrightenedColor(color));
+            }
+        }
+
+        public void SetColor(Color color)
+        {
+            if (colorAnimator != null)
+            {
+                StopCoroutine(colorAnimator);
+            }
+
+            ChangeColorProperties(color);
+        }
+
+        public void SetColor(Color color, float animationDuration)
+        {
+            if (colorAnimator != null)
+            {
+                StopCoroutine(colorAnimator);
+            }
+
+            if (animationDuration <= float.Epsilon)
+            {
+                SetColor(color);
+            }
+            else
+            {
+                colorAnimator = StartCoroutine(
+                    BlendColorsAnimator(color, animationDuration));
+            }
+        }
+
+        IEnumerator BlendColorsAnimator(Color color, float animationDuration)
+        {
+            float elapsedTime = 0;
+            while (elapsedTime < animationDuration)
+            {
+                ChangeColorProperties(
+                    Color.Lerp(
+                        rend.material.color,
+                        color,
+                        Time.deltaTime / (animationDuration - elapsedTime)));
+
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            ChangeColorProperties(color);
+            colorAnimator = null;
+        }
+
+        #endregion
+    }
+}
