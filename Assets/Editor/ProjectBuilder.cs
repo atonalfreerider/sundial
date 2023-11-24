9fffc3cefdfd3b883d237ddec2484365cf03ecd3
john <john@primitive.io>
2023-11-24 13:37:47 -0800

    upgrade Unity 2023.2.1f1

--- Assets/Editor/ProjectBuilder.cs
+++ Assets/Editor/ProjectBuilder.cs
@@ -22,7 +22,7 @@ namespace Assets.Editor
             Beebyte.Obfuscator.OptionsManager.LoadOptions().enabled = true;
             PlayerSettings.productName = "Sundial";
 
-            BuildAndroid(BuildOptions.None, AndroidSdkVersions.AndroidApiLevel22, false);
+            BuildAndroid(BuildOptions.None, AndroidSdkVersions.AndroidApiLevel23, false);
         }
         
         [MenuItem("Build/Build Pro Release")]
@@ -31,7 +31,7 @@ namespace Assets.Editor
             Beebyte.Obfuscator.OptionsManager.LoadOptions().enabled = true;
             PlayerSettings.productName = "SundialPro";
 
-            BuildAndroid(BuildOptions.None, AndroidSdkVersions.AndroidApiLevel22, true);
+            BuildAndroid(BuildOptions.None, AndroidSdkVersions.AndroidApiLevel23, true);
         }
 
         static void BuildAndroid(
