3f6932df6f846af499ccc9cc049af441bc55837a
John <john@primitive.io>
2024-04-20 13:08:46 -0700

    upgrade min android to API 30
    https://support.google.com/googleplay/android-developer/answer/11926878?hl=en

--- Assets/Editor/ProjectBuilder.cs
+++ Assets/Editor/ProjectBuilder.cs
@@ -22,7 +22,7 @@ namespace Assets.Editor
             Beebyte.Obfuscator.OptionsManager.LoadOptions().enabled = true;
             PlayerSettings.productName = "Sundial";
 
-            BuildAndroid(BuildOptions.None, AndroidSdkVersions.AndroidApiLevel23, false);
+            BuildAndroid(BuildOptions.None, AndroidSdkVersions.AndroidApiLevel30, false);
         }
         
         [MenuItem("Build/Build Pro Release")]
@@ -31,7 +31,7 @@ namespace Assets.Editor
             Beebyte.Obfuscator.OptionsManager.LoadOptions().enabled = true;
             PlayerSettings.productName = "SundialPro";
 
-            BuildAndroid(BuildOptions.None, AndroidSdkVersions.AndroidApiLevel23, true);
+            BuildAndroid(BuildOptions.None, AndroidSdkVersions.AndroidApiLevel30, true);
         }
 
         static void BuildAndroid(
